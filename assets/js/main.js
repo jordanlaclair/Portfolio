3b6f9edd6c776b378393a39a8cf158a64073540b
Jordan Molina <jordanmichaelmolina3@gmail.com>
2021-09-15 20:53:21 -0500

    scroll reveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,8 +28,6 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
-	origin: "top",
-	distance: "80px",
 	duration: 1400,
 	reset: true,
 });
